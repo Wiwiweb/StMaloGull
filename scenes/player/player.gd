cd166dd6a364eabb790b22f6c7d79d666daeb815
Wiwiweb <wiwiweb2@gmail.com>
2026-01-19 19:42:44 -0800

    Super basic movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
-@export var forward_velocity : float = 5.0
+@export var forward_velocity : float
+@export var max_turn_rate : float
 
-func _physics_process(_delta: float) -> void:
-	velocity.z = -forward_velocity
+func _physics_process(delta: float) -> void:
+	var joystick_vector := Input.get_vector("move_left", "move_right", "move_down", "move_up")
+	rotation.y = rotation.y + -joystick_vector.x * max_turn_rate * delta
+	rotation.z = rotation.z + joystick_vector.y * max_turn_rate * delta
+
+	velocity = basis.x * forward_velocity
 	move_and_slide()
   
